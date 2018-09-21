b5882cee99df4dc25419ce39b55ddfd56c14d8ca
Maxmya <maxmya@outlook.com>
2018-09-20 21:45:14 -0400

    init android application

--- app/src/main/java/com/graduation/academic/as/App.java
+++ app/src/main/java/com/graduation/academic/as/App.java
@@ -1,4 +1,15 @@
 package com.graduation.academic.as;
 
-public class App {
+import android.support.multidex.MultiDexApplication;
+import android.widget.Toast;
+
+public class App extends MultiDexApplication {
+
+    private static final String TAG = App.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, TAG + "initialized", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/graduation/academic/as/activities/SplashActivity.java
+++ app/src/main/java/com/graduation/academic/as/activities/SplashActivity.java
@@ -2,12 +2,21 @@ package com.graduation.academic.as.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.graduation.academic.as.App;
+import com.graduation.academic.as.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Toast.makeText(this, TAG + "is on", Toast.LENGTH_SHORT).show();
+
     }
+
 }
