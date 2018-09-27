4d7c8bb2cbfa5d005ccdc50fec3075eb1ea22377
Omar Wael <omar_wael1890@yahoo.com>
2018-09-27 16:17:16 +0200

    asyc task

--- app/src/main/java/com/graduation/academic/as/activities/SplashActivity.java
+++ app/src/main/java/com/graduation/academic/as/activities/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.graduation.academic.as.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
@@ -10,6 +13,7 @@ import com.graduation.academic.as.R;
 import com.graduation.academic.as.api.Api;
 import com.graduation.academic.as.api.ApiHelper;
 import com.graduation.academic.as.api.MultipleResource;
+
 
 import java.util.List;
 
@@ -29,42 +33,54 @@ public class SplashActivity extends AppCompatActivity {
         // this is how to create instance of api interface
         api = ApiHelper.getClient().create(Api.class);
         responseText = (TextView) findViewById(R.id.responseText);
-
-        /**
-         GET List Resources
-         **/
-        retrofit2.Call<MultipleResource> call = api.doGetListResources();
-        call.enqueue(new Callback<MultipleResource>() {
-            @Override
-            public void onResponse(retrofit2.Call<MultipleResource> call, Response<MultipleResource> response) {
-                Log.d("TAG", response.code() + "");
-
-                String displayResponse = "";
-
-                MultipleResource resource = response.body();
-                Integer text = resource.page;
-                Integer total = resource.total;
-                Integer totalPages = resource.totalPages;
-                List<MultipleResource.Datum> datumList = resource.data;
-
-                displayResponse += text + " Page\n" + total + " Total\n" + totalPages + " Total Pages\n";
-
-                for (MultipleResource.Datum datum : datumList) {
-                    displayResponse += datum.id + " " + datum.name + " " + datum.pantoneValue + " " + datum.year + "\n";
-                }
-
-                responseText.setText(displayResponse);
-
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<MultipleResource> call, Throwable t) {
-
-                call.cancel();
-            }
-        });
+        ConntectToServer cts = new ConntectToServer();
+        cts.execute();
         }
 
+    class  ConntectToServer extends AsyncTask {
+
+        @Override
+        protected String doInBackground(Object[] objects) {
+            /**
+             GET List Resources
+             **/
+            retrofit2.Call<MultipleResource> call = api.doGetListResources();
+            call.enqueue(new Callback<MultipleResource>() {
+                @Override
+                public void onResponse(retrofit2.Call<MultipleResource> call, Response<MultipleResource> response) {
+
+                    Log.d("TAG", response.code() + "");
+                    String displayResponse = "";
+                    MultipleResource resource = response.body();
+                    Integer text = resource.page;
+                    Integer total = resource.total;
+                    Integer totalPages = resource.totalPages;
+                    List<MultipleResource.Datum> datumList = resource.data;
+
+                    displayResponse += text + " Page\n" + total + " Total\n" + totalPages + " Total Pages\n";
+
+                    for (MultipleResource.Datum datum : datumList) {
+                        displayResponse += datum.id + " " + datum.name + " " + datum.pantoneValue + " " + datum.year + "\n";
+                    }
+
+                    responseText.setText(displayResponse);
+
+                }
+
+                @Override
+                public void onFailure(retrofit2.Call<MultipleResource> call, Throwable t) {
+
+                    call.cancel();
+                }
+            });
+            return null;
+        }
+
+//        @Override
+//        protected void onPostExecute(Object o) {
+//            super.onPostExecute(o);
+//
+//        }
     }
 
-
+}
