34e9eaf578a362d64f2fafacb23bf5bd5641b11b
Omar Wael <omar_wael1890@yahoo.com>
2018-10-02 00:44:28 +0200

    login xml

---
+++ app/src/main/java/com/graduation/academic/as/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.graduation.academic.as.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.graduation.academic.as.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
